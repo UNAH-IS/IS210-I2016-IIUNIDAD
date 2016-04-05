16d9fa4aca64ea4189117b5fddc08d27deff53b4
Erick Reyes <erick.marin.v@gmail.com>
2016-04-05 09:50:27 -0600

    Formularios Multiples
    Formularios Multiples

--- EjercicioFormulariosMultiples/src/formularios/FormularioPrincipal.java
+++ EjercicioFormulariosMultiples/src/formularios/FormularioPrincipal.java
@@ -1,24 +1,33 @@
 package formularios;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JInternalFrame;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class FormularioPrincipal extends JFrame {
 
-	private JPanel contentPane;
+	private JDesktopPane contentPane;
 	private JMenuItem mntmSalir;
-	private JInternalFrame internalFrame;
+	private JInternalFrame frmRegistroMarcas;
 	private JMenuItem mntmRegistrarMarca;
+	private FormularioRegistroTelefonos frmRegistroTelefonos;
+	private JInternalFrame frmRegistroModelos;
+	private JMenuItem mntmRegistrarTelefono;
+	private JMenuItem mntmRegistrarModelo;
+	private JToolBar toolBar;
+	private JButton btnAbrir;
 
 	/**
 	 * Launch the application.
@@ -33,41 +42,69 @@ public class FormularioPrincipal extends JFrame {
 	 */
 	public FormularioPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 334);
-		
+		setBounds(100, 100, 900, 600);
+				
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
 		JMenu mnFile = new JMenu("Operaciones");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmOpen = new JMenuItem("Registrar Telefono");
-		mnFile.add(mntmOpen);
+		mntmRegistrarTelefono = new JMenuItem("Registrar Telefono");
+		mnFile.add(mntmRegistrarTelefono);
 		
 		mntmRegistrarMarca = new JMenuItem("Registrar Marca");
 		mnFile.add(mntmRegistrarMarca);
 		
-		JMenuItem mntmRegistrarModelo = new JMenuItem("Registrar Modelo");
+		mntmRegistrarModelo = new JMenuItem("Registrar Modelo");
 		mnFile.add(mntmRegistrarModelo);
 		
 		mntmSalir = new JMenuItem("Salir");
 		mnFile.add(mntmSalir);
-		contentPane = new JPanel();
+		contentPane = new JDesktopPane();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		internalFrame = new JInternalFrame("Registrar Marca");
-		internalFrame.setMaximizable(true);
-		internalFrame.setIconifiable(true);
-		internalFrame.setClosable(true);
-		internalFrame.setBounds(10, 11, 282, 198);
-		contentPane.add(internalFrame);
+		frmRegistroMarcas = new JInternalFrame("Registrar Marca");
+		frmRegistroMarcas.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frmRegistroMarcas.setMaximizable(true);
+		frmRegistroMarcas.setIconifiable(true);
+		frmRegistroMarcas.setClosable(true);
+		frmRegistroMarcas.setBounds(461, 45, 282, 198);
+		contentPane.add(frmRegistroMarcas);
+		
+		frmRegistroModelos = new JInternalFrame("Registrar Modelos");
+		frmRegistroModelos.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frmRegistroModelos.setClosable(true);
+		frmRegistroModelos.setIconifiable(true);
+		frmRegistroModelos.setMaximizable(true);
+		frmRegistroModelos.setBounds(10, 338, 275, 198);
+		contentPane.add(frmRegistroModelos);
+		
+		frmRegistroTelefonos = new FormularioRegistroTelefonos();
+		frmRegistroTelefonos.setMaximizable(true);
+		frmRegistroTelefonos.setIconifiable(true);
+		frmRegistroTelefonos.setClosable(true);
+		contentPane.add(frmRegistroTelefonos);
+		
+		toolBar = new JToolBar();
+		toolBar.setBounds(0, 0, 884, 34);
+		contentPane.add(toolBar);
+		
+		btnAbrir = new JButton("");
+		btnAbrir.setIcon(new ImageIcon(FormularioPrincipal.class.getResource("/recursos/abrir.png")));
+		toolBar.add(btnAbrir);
 		
 		gestionEventos();
 	}
 	
 	public void gestionEventos(){
+		btnAbrir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Abrir");
+			}
+		});
 		mntmSalir.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -78,8 +115,26 @@ public class FormularioPrincipal extends JFrame {
 		mntmRegistrarMarca.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				internalFrame.setVisible(true);
+				frmRegistroMarcas.setLocation(0, 0);
+				frmRegistroMarcas.setVisible(true);
 			}
 		});
+		
+		mntmRegistrarModelo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				frmRegistroModelos.setLocation(0, 0);
+				frmRegistroModelos.setVisible(true);
+			}
+		});
+		
+		mntmRegistrarTelefono.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				frmRegistroTelefonos.setLocation(0, 0);
+				frmRegistroTelefonos.setVisible(true);
+			}
+		});
+		
 	}
 }
